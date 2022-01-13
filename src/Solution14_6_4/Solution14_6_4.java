66d429fe775ee397d3a7c858c138251d54c4e6e6
MikeKrysan <krysanmihasan@gmail.com>
2022-01-13 14:11:28 +0200

    third commit - Solution14_6_4 fixed

--- src/Solution14_6_4/Solution14_6_4.java
+++ src/Solution14_6_4/Solution14_6_4.java
@@ -23,22 +23,17 @@ import java.util.List;
 Теперь запишите содержимое этого файла в другой файл со строками, записанными в обратном порядке.
  */
 public class Solution14_6_4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Path path = Paths.get("src\\Solution14_6_4\\newFile.txt");   //берем существующий файл
-        List<String> contents = null;   //читаем в лист памяти программы
-        try {
-            contents = Files.readAllLines(path);
-        } catch (IOException e) {
-            System.out.println("Указанный файл не найден!");
-        }
+        List<String> contents = Files.readAllLines(path);   //читаем в лист памяти программы
 
-        try {
-            BufferedWriter writer = Files.newBufferedWriter(Paths.get("src\\Solution14_6_4\\newFile-reversed.txt")) ;{    //пишем в новый файл
+
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("src\\Solution14_6_4\\newFile-reversed.txt"))) {    //пишем в новый файл
                 for (int index = contents.size() - 1; index >= 0; index--) {
                     writer.write(contents.get(index));
                     writer.write("\n"); //не забываем про перенос строки
                 }
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
